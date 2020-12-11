15674c8191b0dd62c1ab6280e3842d749cdcd18b
jcarr98 <jjcarr@umas.edu>
2020-12-10 19:05:10 -0500

    Cleaned runClient more

--- SecureExchange.py
+++ SecureExchange.py
@@ -68,76 +68,83 @@ def greeting():
 
 
 def runClient(user):
+    """Hub that redirects code to appropriate command
+    
+            Parameters:
+                user (User): The user to perform actions on
+    """
     # Loop until quit
     while True:
         # List options for user
         print("What would you like to do?")
         print("(1) Send, (2) Refresh, (3) List Messages, (4) Read Message, (5) Quit")
-        userInput = input("Enter an option number: ")
+        cmd = input("Enter an option number: ")
 
-        # Check for valid input
-        try:
-            userInput = int(userInput)
-        except ValueError:
-            print("Invalid option, please enter the number only.")
-            continue
-        
-        #Clear screen
-        clear()
+        choices = {
+            "1": send,
+            "2": refresh,
+            "3": list_messages,
+            "4": list_messages,
+            "5": quit
+        }
 
-        # Parse user input
-        if userInput == 1:
-            while True:
-                recipient = input("Who would you like to send the file to? ")
-                # Check recipient exists - BACKEND FUNCTION
-                path = "%s/test_files" % os.getcwd()
-                allFiles = os.listdir(path)
-                exists = False
-                for i in allFiles:
-                    if os.path.isdir("%s/%s" % (path, i)) and i == recipient:
-                            exists = True
-                            break
-                if exists:
+        action = choices(cmd, lambda: "Invalid input. Please enter the number only.")
+
+        action()
+
+def send(user):
+    while True:
+        recipient = input("Who would you like to send the file to? ")
+        # Check recipient exists - BACKEND FUNCTION
+        path = "%s/test_files" % os.getcwd()
+        allFiles = os.listdir(path)
+        exists = False
+        for i in allFiles:
+            if os.path.isdir("%s/%s" % (path, i)) and i == recipient:
+                    exists = True
                     break
-                else:
-                    print("Please pick a valid recipient")
-            while True:
-                fileType = input("Would you like to send a message or a file? m/f: ")
-                print(fileType)
-                if fileType != "m" and fileType != "f":
-                    print("Please pick a valid option.")
-                else:
-                    break
-
-            if fileType == "m":
-                while True:
-                    subject = input("What is the subject of your message? ")
-                    if len(subject.strip(" ")) == 0:
-                        print("Please enter a valid subject")
-                    else:
-                        break
-
-                print("What message would you like to send?")
-                message = input()
-                user.send(recipient, subject, message, True)
-            else:
-                print("Work in progress")
-        elif userInput == 2:
-            user.receive()
-        elif userInput == 3:
-            messages = user.getMessages()
-            if len(messages) == 0:
-                print("No messages!")
-                continue
-            for i in range(0, len(messages)):
-                print("%i. %s from %s" % (i+1, messages[i]["subject"], messages[i]["sender"]))
-        elif userInput == 4:
-            user.readMessage()
-        elif userInput == 5:
-            print("Goodbye!")
-            sys.exit()
+        if exists:
+            break
         else:
-            print("Invalid option, please enter a number from above.")
+            print("Please pick a valid recipient")
+    while True:
+        fileType = input("Would you like to send a message or a file? m/f: ")
+        print(fileType)
+        if fileType != "m" and fileType != "f":
+            print("Please pick a valid option.")
+        else:
+            break
+
+    if fileType == "m":
+        while True:
+            subject = input("What is the subject of your message? ")
+            if len(subject.strip(" ")) == 0:
+                print("Please enter a valid subject")
+            else:
+                break
+
+        print("What message would you like to send?")
+        message = input()
+        user.send(recipient, subject, message, True)
+    else:
+        print("Work in progress")
+
+def refresh(user):
+    user.receive()
+
+def list_messages(user):
+    messages = user.getMessages()
+    if len(messages) == 0:
+        print("No messages!")
+    for i in range(0, len(messages)):
+        print("%i. %s from %s" % (i+1, messages[i]["subject"], messages[i]["sender"]))
+
+def read_message(user):
+    user.readMessage()
+
+def quit():
+    print("Goodbye!")
+    sys.exit()
 
 
 if __name__ == "__main__":
